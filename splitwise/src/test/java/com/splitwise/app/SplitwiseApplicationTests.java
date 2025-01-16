20abb60336192e27819ba18a9a952c1dc645f4bc
ashu yadav <651ashutoshyadav@gmail.com>
2025-01-16 21:46:37 +0530

    first commit

---
+++ splitwise/src/test/java/com/splitwise/app/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwise.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
